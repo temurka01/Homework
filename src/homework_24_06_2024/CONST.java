29012898ec667de4240709373f64ffec4a3b87a3
temurka <temurka0@yandex.ru>
2024-06-24 18:37:23 +0300

    10

---
+++ src/homework_24_06_2024/CONST.java
@@ -0,0 +1,12 @@
+package homework_24_06_2024;
+
+public class CONST {
+    public static final Program[] PROGRAMS = {new Program("Программа 1"),
+            new Program("Программа 2"),
+            new Program("Программа 3"),
+            new Program("Программа 4"),
+            new Program("Программа 5"),
+            new Program("Программа 6"),
+            new Program("программа 7"),
+            new Program("Программа 8")};
+}
