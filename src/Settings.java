40d69a20ddfb93380d9b498298c1b7370f93cb16
temurka <temurka0@yandex.ru>
2024-07-19 21:53:44 +0300

    3

---
+++ src/Settings.java
@@ -0,0 +1,6 @@
+
+public class Settings {
+    public static final int WINDOW_WIGHT = 1000;
+    public static final int WINDOW_HEIGHT = 1200;
+    public static final int CELL_WIGHT_HEIGHT = 40;
+}
